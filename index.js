7cbd07d37073617c5215de1ce94dcd0f738f3142
William Huang <shsidforever@gmail.com>
2019-05-17 22:52:34 +0800

    heh

--- index.js
+++ index.js
@@ -18,20 +18,15 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post("/callback", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then(result => res.json(result))
-    .catch(err => {
-      console.error(err);
-      res.status(500).end();
-    });
+app.post("/callback", (req, res) => {
+  res.status(200);
 });
 
 // event handler
 function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     // ignore non-text-message event
-    return Promise.resolve({ status: 200 });
+    return Promise.resolve(null);
   }
 
   // create a echoing text message
