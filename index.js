1d807c729b7661fdc3fe04dbededbbd6d8a43085
William Huang <shsidforever@gmail.com>
2019-05-17 22:45:13 +0800

    verification p2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const line = require('@line/bot-sdk');
-const express = require('express');
+const line = require("@line/bot-sdk");
+const express = require("express");
 
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 // create LINE SDK client
@@ -18,25 +18,24 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
       console.error(err);
-      res.status(200).end();
+      res.status(500).end();
     });
 });
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (event.type !== "message" || event.message.type !== "text") {
     // ignore non-text-message event
-    return Promise.resolve(null);
+    return Promise.resolve({ status: 200 });
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: "text", text: event.message.text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
